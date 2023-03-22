04735925b787883ab3c7eca212fb9c21c20ea2aa
Ahmad Reza Gollari <ahmadrezagollari@gmail.com>
2023-03-22 16:35:03 +0330

    Comment domain implemented - Application structure created.

---
+++ MB.Domain/CommentAgg/Comment.cs
@@ -0,0 +1,28 @@
+﻿using MB.Domain.ArticleAgg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MB.Domain.CommentAgg;
+public class Comment
+{
+    public int Id { get; private set; }
+    public string Name { get; private set; }
+    public string Email { get; private set; }
+    public string Message { get; private set; }
+    public DateTime CreationDate { get; private set; } = DateTime.Now;
+    public int Status { get; private set; } = Statuses.New;
+    public long ArticleId { get; private set; }
+    public Article Article { get; private set; }
+
+    protected Comment() { }
+    public Comment(string name, string email, string message, long articleId)
+    {
+        Name = name;
+        Email = email;
+        Message = message;
+        ArticleId = articleId;
+    }
+}
---
+++ MB.Domain/CommentAgg/Statuses.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MB.Domain.CommentAgg;
+public static class Statuses
+{
+    public const int New = 0;
+    public const int Confirmed = 1;
+    public const int Canceled = 2;
+}
